8ec29b0a8e79bafed0873072511cef44605bdf83
Evgenij Pavlov <evgenij.pavlov@filuet.ru>
2022-03-12 16:58:35 +0300

    Fix IGetRepository namespace

--- warehouse-challenge-backend/Domain/Common/Abstractions/Repository/IGetRepository.cs
+++ warehouse-challenge-backend/Domain/Common/Abstractions/Repository/IGetRepository.cs
@@ -4,17 +4,14 @@ using System.Threading.Tasks;
 
 namespace Domain.Common.Abstractions.Repository
 {
-    public interface IGetRepository
+    /// <summary>
+    /// Represents some repository for retrieving data ('get' operations)
+    /// </summary>
+    /// <typeparam name="TEntity">Entity type</typeparam>
+    /// <typeparam name="TIdentifier">Some entity idtifier type (string\int\etc)</typeparam>
+    public interface IGetRepository<TEntity, TIdentifier> where TEntity : IEntity
     {
-        /// <summary>
-        /// Represents some repository for retrieving data ('get' operations)
-        /// </summary>
-        /// <typeparam name="TEntity">Entity type</typeparam>
-        /// <typeparam name="TIdentifier">Some entity idtifier type (string\int\etc)</typeparam>
-        public interface IGetRepository<TEntity, TIdentifier> where TEntity : IEntity
-        {
-            public Task<IEnumerable<TEntity>> GetAllAsync();
-            public Task<TEntity> GetAsync(TIdentifier id);
-        }
+        public Task<IEnumerable<TEntity>> GetAllAsync();
+        public Task<TEntity> GetAsync(TIdentifier id);
     }
 }
