95a752f1e7fc1e1506f78b0af6957cce52a0eb0f
Evgenij Pavlov <evgenij.pavlov@filuet.ru>
2022-03-12 17:48:26 +0300

    Added warehouse repository interface

---
+++ warehouse-challenge-backend/Domain/VehicleWarehouse/Abstractions/Repository/IWarehouseRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Common.Abstractions.Repository;
+using Domain.VehicleWarehouse.Abstractions.Entities;
+
+namespace Domain.VehicleWarehouse.Abstractions.Repository
+{
+    public interface IWarehouseRepository : IGetRepository<Warehouse, string>
+    {
+    }
+}
