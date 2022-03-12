9c6ed3f09678de26665ebf2bc81741e4c97c338b
Evgenij Pavlov <evgenij.pavlov@filuet.ru>
2022-03-12 17:50:26 +0300

    Added warehouse service

---
+++ warehouse-challenge-backend/Domain/VehicleWarehouse/Domain.VehicleWarehouse.Services/Service/WarehouseService.cs
@@ -0,0 +1,31 @@
+﻿using Domain.VehicleWarehouse.Abstractions.Entities;
+using Domain.VehicleWarehouse.Abstractions.Repository;
+using Domain.VehicleWarehouse.Abstractions.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.VehicleWarehouse.Services.Service
+{
+    public class WarehouseService : IWarehouseService
+    {
+        private readonly IWarehouseRepository _warehouseRepository;
+
+        public WarehouseService (IWarehouseRepository warehouseRepository) : base()
+        {
+            _warehouseRepository = warehouseRepository;
+        }
+
+        public async Task<IEnumerable<Warehouse>> GetAllAsync()
+        {
+            return await _warehouseRepository.GetAllAsync();
+        }
+
+        public async Task<Warehouse> GetAsync(object id)
+        {
+            return await _warehouseRepository.GetAsync((string)id);
+        }
+    }
+}
