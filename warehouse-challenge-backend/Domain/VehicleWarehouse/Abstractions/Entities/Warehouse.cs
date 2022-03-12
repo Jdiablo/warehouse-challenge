660914023f5f5c1630a75e14c8c72f3eca08d034
Evgenij Pavlov <evgenij.pavlov@filuet.ru>
2022-03-12 17:26:54 +0300

    Added warehouse entity

---
+++ warehouse-challenge-backend/Domain/VehicleWarehouse/Abstractions/Entities/Warehouse.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Common.Abstractions.Entities;
+
+namespace Domain.VehicleWarehouse.Abstractions.Entities
+{
+    public class Warehouse : IdentifiableEntity<string>
+    {
+        public string Name { get; set; }
+
+        public string LocationLat { get; set; }
+
+        public string LocationLong { get; set; }
+
+        public WarehouseWing Cars { get; set; }
+    }
+}
---
+++ warehouse-challenge-backend/Domain/VehicleWarehouse/Abstractions/Entities/WarehouseWing.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Common.Abstractions.Entities;
+using System.Collections.Generic;
+
+namespace Domain.VehicleWarehouse.Abstractions.Entities
+{
+    public class WarehouseWing : IEntity
+    {
+        public string Name { get; set; }
+
+        public IEnumerable<Vehicle> Vehicles { get; set; }
+    }
+}
