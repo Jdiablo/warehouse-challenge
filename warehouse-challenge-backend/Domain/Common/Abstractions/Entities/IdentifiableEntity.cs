90400acfabee60d0952f047fc351bcd610be47b9
Evgenij Pavlov <evgenij.pavlov@filuet.ru>
2022-03-12 16:39:32 +0300

    Added common abstractions project, entities

---
+++ warehouse-challenge-backend/Domain/Common/Abstractions/Entities/IdentifiableEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.VehicleWarehouse.Abstractions.Entities
+{
+    public abstract class IdentifiableEntity<TIdentity> : IEntity
+    {
+        public virtual TIdentity Id { get; set; }
+    }
+}
