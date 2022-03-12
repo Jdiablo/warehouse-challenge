e6ac564025bea38d785b389680b42bba2ac12499
Evgenij Pavlov <evgenij.pavlov@filuet.ru>
2022-03-12 16:42:28 +0300

    Added common GET repository interface

---
+++ warehouse-challenge-backend/Domain/Common/Abstractions/Repository/IGetRepository.cs
@@ -0,0 +1,23 @@
+﻿using Domain.VehicleWarehouse.Abstractions.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.VehicleWarehouse.Abstractions.Repository
+{
+    public interface IGetRepository
+    {
+        /// <summary>
+        /// Represents some repository for retrieving data ('get' operations)
+        /// </summary>
+        /// <typeparam name="TEntity">Entity type</typeparam>
+        /// <typeparam name="TIdentifier">Some entity idtifier type (string\int\etc)</typeparam>
+        public interface IGetRepository<TEntity, TIdentifier> where TEntity : IEntity
+        {
+            public Task<IEnumerable<TEntity>> GetAllAsync();
+            public Task<TEntity> GetAsync(TIdentifier id);
+        }
+    }
+}
