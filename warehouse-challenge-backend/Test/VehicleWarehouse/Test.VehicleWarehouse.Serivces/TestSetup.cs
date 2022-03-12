c16a3204edab722eabab4436c2a12145cac0569b
Evgenij Pavlov <evgenij.pavlov@filuet.ru>
2022-03-12 19:17:39 +0300

    Added warehouse service tests

---
+++ warehouse-challenge-backend/Test/VehicleWarehouse/Test.VehicleWarehouse.Serivces/TestSetup.cs
@@ -0,0 +1,50 @@
+﻿using Domain.Common.Abstractions.Entities;
+using Domain.Common.Abstractions.Repository;
+using Domain.VehicleWarehouse.Abstractions.Entities;
+using Domain.VehicleWarehouse.Abstractions.Repository;
+using Domain.VehicleWarehouse.Services.Service;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.VehicleWarehouse.Serivces
+{
+    public class TestSetup
+    {
+        //private T SetupGetRepo<T, TEntity, TIdentifier>() where T : IGetRepository<TEntity, TIdentifier>
+        //        where TEntity : IEntity
+        //        where TIdentifier : class
+        //{
+        //    var mockedRepo = new Mock<T<TEntity, TIdentifier>>();
+        //    mockedRepo.Setup(x => x.GetAllAsync()).Returns(() =>
+        //    {
+        //        var list = new List<IEntity>();
+        //        list.Add(Moq.It.IsAny<IEntity>());
+        //        return Task.FromResult(list as IEnumerable<TEntity>);
+        //    });
+        //    return mockedRepo.Object;
+        //}
+
+        private IWarehouseRepository SetupWarehouseRepo()
+        {
+            var repo = new Mock<IWarehouseRepository>();
+            repo.Setup(x => x.GetAllAsync()).Returns(() =>
+            {
+                var list = new List<Warehouse>();
+                list.Add(Moq.It.IsAny<Warehouse>());
+                return Task.FromResult(list as IEnumerable<Warehouse>);
+            });
+            repo.Setup(x => x.GetAsync(It.IsAny<string>())).Returns(() => Task.FromResult(new Warehouse()));
+
+            return repo.Object;
+        }
+
+        public WarehouseService SetupWarehouseService()
+        {
+            return new WarehouseService(SetupWarehouseRepo());
+        }
+    }
+}
---
+++ warehouse-challenge-backend/Test/VehicleWarehouse/Test.VehicleWarehouse.Serivces/WarehouseServiceTest.cs
@@ -0,0 +1,32 @@
+using Domain.VehicleWarehouse.Abstractions.Repository;
+using Domain.VehicleWarehouse.Abstractions.Service;
+using Domain.VehicleWarehouse.Services.Service;
+using Moq;
+using System;
+using Xunit;
+
+namespace Test.VehicleWarehouse.Serivces
+{
+    public class WarehouseServiceTest
+    {
+        private readonly IWarehouseService warehouseService = new TestSetup().SetupWarehouseService();
+
+        [Fact]
+        public void GetAllWarehouses()
+        {
+            var result = warehouseService.GetAllAsync().Result;
+
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+        }
+
+
+        [Fact]
+        public void GetWarehouse()
+        {
+            var result = warehouseService.GetAsync("test").Result;
+
+            Assert.NotNull(result);
+        }
+    }
+}
