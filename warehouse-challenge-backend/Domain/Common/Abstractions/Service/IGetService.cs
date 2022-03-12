2b9be16d509b71a05581cce9909b6ad8319ac64d
Evgenij Pavlov <evgenij.pavlov@filuet.ru>
2022-03-12 17:31:51 +0300

    Added GET service interface

---
+++ warehouse-challenge-backend/Domain/Common/Abstractions/Service/IGetService.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Common.Abstractions.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Domain.Common.Abstractions.Service
+{
+    /// <summary>
+    /// Represents some service for retrieving data ('get' operations)
+    /// </summary>
+    /// <typeparam name="TEntity">Entity type</typeparam>
+    public interface IGetService<TEntity> where TEntity : IEntity
+    {
+        public Task<IEnumerable<TEntity>> GetAllAsync();
+        public Task<TEntity> GetAsync(object id);
+    }
+}
