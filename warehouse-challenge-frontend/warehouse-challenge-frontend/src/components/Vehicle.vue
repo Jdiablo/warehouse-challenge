5b980dc44fdbd99094af3c48154f7a9c0af05dbe
Evgenij Pavlov <evgenij.pavlov@filuet.ru>
2022-03-13 15:05:28 +0300

    Added vehicle component

---
+++ warehouse-challenge-frontend/warehouse-challenge-frontend/src/components/Vehicle.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="mb-3 row">
+        <label>Make</label>
+        <input type="text" class="form-control" v-model="currentVehicle.make" :disabled="isEditEnabled == false" />
+    </div>
+    <div class="mb-3 row">
+        <label>Model</label>
+        <input type="text" class="form-control" v-model="currentVehicle.model" :disabled="isEditEnabled == false" />
+    </div>
+    <div class="mb-3 row">
+        <label>Licensed</label>
+        <input type="checkbox" class="form-control" v-model="currentVehicle.licensed" :disabled="isEditEnabled == false" />
+    </div>
+    <div class="mb-3 row">
+        <label>Year model</label>
+        <input type="number" class="form-control" v-model="currentVehicle.yearModel" :disabled="isEditEnabled == false" />
+    </div>
+    <div class="mb-3 row">
+        <label>Price</label>
+        <input type="number" class="form-control" v-model="currentVehicle.price" :disabled="isEditEnabled == false" />
+    </div>
+    <div class="mb-3 row">
+        <label>Date added</label>
+        <input type="number" class="form-control" v-model="currentVehicle.dateAdded" disabled="disabled" />
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from "vue";
+    import type Vehicle from "../types/Vehicle";
+
+    export default defineComponent({
+        data() {
+            return {
+                currentVehicle: {} as Vehicle
+            }
+        },
+        props: {
+            isEditEnabled: Boolean
+        }
+    });
+</script>
+
+<style scoped>
+</style>
