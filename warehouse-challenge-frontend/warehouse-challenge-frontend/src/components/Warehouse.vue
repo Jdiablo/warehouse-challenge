b96a70503727be6eb86537f7d66a416803fa77b7
Evgenij Pavlov <evgenij.pavlov@filuet.ru>
2022-03-13 17:46:41 +0300

    Adde warehouse component

---
+++ warehouse-challenge-frontend/warehouse-challenge-frontend/src/components/Warehouse.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="mb-3 row">
+        <label class="col-2">Name</label>
+        <input class="col-10 form-control" type="text" v-model="currentWarehouse.name" :disabled="isEditEnabled == false" />
+    </div>
+    <div class="mb-3 row">
+        <label class="col-2">Lat</label>
+        <input class="col-4 form-control" type="text" v-model="currentWarehouse.locationLat" :disabled="isEditEnabled == false" />
+        <label class="col-2">Lon</label>
+        <input class="col-4 form-control" type="text" v-model="currentWarehouse.locationLong" :disabled="isEditEnabled == false" />
+    </div>
+    <div>
+        <WarehouseWing v-bind:currentWing="currentWarehouse.warehouseWing"></WarehouseWing>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent, PropType } from "vue"
+    import WarehouseWing from "./WarehouseWing.vue"
+    import type WarehouseModel from "@/types/Warehouse"
+
+    export default defineComponent({
+        components: {
+            WarehouseWing
+        },
+        props: {
+            currentWarehouse: {
+                type: Object as PropType<WarehouseModel>,
+                required: true
+            },
+            isEditEnabled: Boolean
+        }
+    });
+</script>
+
+<style scoped>
+</style>
