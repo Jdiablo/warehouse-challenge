e9107764c48b68beaad11d4f2d69b7eab70f9176
Evgenij Pavlov <evgenij.pavlov@filuet.ru>
2022-03-13 16:36:11 +0300

    Added missing packages

--- warehouse-challenge-frontend/warehouse-challenge-frontend/src/router.ts
+++ warehouse-challenge-frontend/warehouse-challenge-frontend/src/router.ts
@@ -1,11 +1,11 @@
 import { createWebHistory, createRouter } from "vue-router";
-import { RouteRecordRaw } from "vue-router";
+import type { RouteRecordRaw } from "vue-router";
 const routes: Array<RouteRecordRaw> = [
     {
         path: "/",
         alias: "/warehouse",
         name: "warehouse",
-        component: () => import("./components/WarehousesList.vue"),
+        component: () => import("@/components/WarehousesList.vue"),
     }
 ];
 const router = createRouter({
