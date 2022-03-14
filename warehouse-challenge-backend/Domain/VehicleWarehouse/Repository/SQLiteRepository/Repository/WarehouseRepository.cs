7ba442ff94eae147e337a7e90e958fd15bc034e7
Evgenij Pavlov <evgenij.pavlov@filuet.ru>
2022-03-14 23:49:25 +0300

    Select vehicle projection in warehouse GetAll

--- warehouse-challenge-backend/Domain/VehicleWarehouse/Repository/SQLiteRepository/Repository/WarehouseRepository.cs
+++ warehouse-challenge-backend/Domain/VehicleWarehouse/Repository/SQLiteRepository/Repository/WarehouseRepository.cs
@@ -1,7 +1,9 @@
 ﻿using Domain.VehicleWarehouse.Abstractions.Entities;
 using Domain.VehicleWarehouse.Abstractions.Repository;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Domain.VehicleWarehouse.SQLiteRepository.Repository
 {
@@ -14,5 +16,29 @@ namespace Domain.VehicleWarehouse.SQLiteRepository.Repository
         protected override IQueryable<Warehouse> ShortQueryable => base.ShortQueryable
             .Include(x => x.WarehouseWing)
                 .ThenInclude(x => x.Vehicles);
+
+        public override async Task<IEnumerable<Warehouse>> GetAllAsync()
+        {
+            return await ShortQueryable.Select(x => new Warehouse()
+            {
+                Id = x.Id,
+                LocationLat = x.LocationLat,
+                LocationLong = x.LocationLong,
+                Name = x.Name,
+                WarehouseWing = new WarehouseWing()
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Vehicles = x.WarehouseWing.Vehicles.Select(y => new Vehicle()
+                    {
+                        Id = y.Id,
+                        DateAdded = y.DateAdded,
+                        Make = y.Make,
+                        Model = y.Model,
+                        Licensed = y.Licensed
+                    })
+                }
+            }).ToListAsync();
+        }
     }
 }
