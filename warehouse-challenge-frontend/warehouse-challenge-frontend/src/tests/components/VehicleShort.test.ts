db36802143b193d8c47f93e659dee3587d662588
Evgenij Pavlov <evgenij.pavlov@filuet.ru>
2022-03-14 19:06:43 +0300

    Added unit test for vehicle short info component

---
+++ warehouse-challenge-frontend/warehouse-challenge-frontend/src/tests/components/VehicleShort.test.ts
@@ -0,0 +1,35 @@
+import { mount } from '@vue/test-utils'
+import { assert, expect, test } from 'vitest'
+import type VehicleShortModel from "@/types/VehicleShortModel"
+import VehicleShort from "@/components/VehicleShort.vue"
+import router from "@/router"
+
+test('renders a vehicle short card info', async () => {
+    let vehicleData = {} as VehicleShortModel;
+
+    const wrapper = mount(VehicleShort, {
+        props: {
+            currentVehicle: vehicleData
+        },
+        global: {
+            plugins: [router]
+        }
+    });
+
+    expect(wrapper.html()).toContain('Model')
+});
+
+test('non licensed car doesnt have Details button', async () => {
+    let vehicleData = { licensed: false } as VehicleShortModel;
+
+    const wrapper = mount(VehicleShort, {
+        props: {
+            currentVehicle: vehicleData
+        },
+        global: {
+            plugins: [router]
+        }
+    });
+
+    expect(wrapper.html()).not.toContain('Details')
+});
--- warehouse-challenge-frontend/warehouse-challenge-frontend/vite.config.ts
+++ warehouse-challenge-frontend/warehouse-challenge-frontend/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     }
   },
     test: {
-        /* for example, use global to avoid globals imports (describe, test, expect): */
-        // globals: true,
+        globals: true,
+        environment: "jsdom",
     },
 })
